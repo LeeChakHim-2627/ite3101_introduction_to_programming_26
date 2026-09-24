a5ddaf6a886408dc000934d10d68b66eeca81b3d
LeeChakHim-2627 <tomlee269c@gmail.com>
2026-09-24 07:55:25 +0000

    [skip actions] lab/lab08/ch08_t14_last_note.py

--- lab/lab08/ch08_t14_last_note.py
+++ lab/lab08/ch08_t14_last_note.py
@@ -12,4 +12,5 @@ inventory['burlap bag'] = ['apple', 'small ruby', 'three-toed sloth']
 inventory['pouch'].sort()
 
 # Your code here
-inventory['pocket'] = ['seashell']
+inventory['pocket'] = ['seashell', 'strange berry', 'lint']
+inventory['backpack'].sort()
