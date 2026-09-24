39500d85c1b4b21539d07ecd99e94ef3d168e2f9
LeeChakHim-2627 <tomlee269c@gmail.com>
2026-09-24 04:02:56 +0000

    [skip actions] lab/lab06/ch06_t06_prac_makes_perfect.py

--- lab/lab06/ch06_t06_prac_makes_perfect.py
+++ lab/lab06/ch06_t06_prac_makes_perfect.py
@@ -1,8 +1,9 @@
 def cube(number: int) -> int:
     return number * number * number
 
-def by_three(number:int)-> Any:
-    if number %3 ==0
+
+def by_three(number: int) -> Any:
+    if number % 3 == 0:
     return cube(number)
     else:
         return False
