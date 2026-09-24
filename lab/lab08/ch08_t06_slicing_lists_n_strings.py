357e84563515c67bf8288eb125827fbab2ac8b2b
LeeChakHim-2627 <tomlee269c@gmail.com>
2026-09-24 07:40:15 +0000

    [skip actions] lab/lab08/ch08_t06_slicing_lists_n_strings.py

--- lab/lab08/ch08_t06_slicing_lists_n_strings.py
+++ lab/lab08/ch08_t06_slicing_lists_n_strings.py
@@ -3,7 +3,7 @@ animals = "catdogfrog"
 # The first three characters of animals
 cat = animals[:3]
 # The fourth through sixth characters
-dog = animals[:]
+dog = animals[3:6]
 
 # From the seventh character to the end
-frog = None
+frog = animals[]
