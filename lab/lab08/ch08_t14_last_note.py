ca31548aa7ca59e54627985598799a33642612b9
LeeChakHim-2627 <tomlee269c@gmail.com>
2026-09-24 07:55:55 +0000

    [skip actions] lab/lab08/ch08_t14_last_note.py

--- lab/lab08/ch08_t14_last_note.py
+++ lab/lab08/ch08_t14_last_note.py
@@ -15,4 +15,4 @@ inventory['pouch'].sort()
 inventory['pocket'] = ['seashell', 'strange berry', 'lint']
 inventory['backpack'].sort()
 inventory['backpack'].remove('dagger')
-in
+inventory['backpack']
