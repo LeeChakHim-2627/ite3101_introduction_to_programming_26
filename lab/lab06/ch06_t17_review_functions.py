6a9493502a479c30ae8559d1534e6e6f89364d84
LeeChakHim-2627 <tomlee269c@gmail.com>
2026-09-24 07:07:30 +0000

    [skip actions] lab/lab06/ch06_t17_review_functions.py

--- lab/lab06/ch06_t17_review_functions.py
+++ lab/lab06/ch06_t17_review_functions.py
@@ -1,7 +1,7 @@
 def stut_down(s:str)->str:
     if s =="yes":
         return "Shutting down"
-    else s =="no":
+    else s == "no":
         return "Shutdown aborted"
     else:
         return "Sorry"
