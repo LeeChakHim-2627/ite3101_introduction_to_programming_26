8689a55da1a93fedd7ce08b1a1b2517b0237a41c
LeeChakHim-2627 <tomlee269c@gmail.com>
2026-09-24 07:51:02 +0000

    [skip actions] lab/lab08/ch08_t11_new_entries.py

--- lab/lab08/ch08_t11_new_entries.py
+++ lab/lab08/ch08_t11_new_entries.py
@@ -3,7 +3,7 @@ menu['Chicken Alfredo'] = 14.50  # Adding new key-value pair
 print(menu['Chicken Alfredo'])
 
 # Your code here: Add some dish-price pairs to menu!
-meuu["Icecream"] = 3
+menu["Icecream"] = 3
 meuu["Spam"] = 2.5
 meuu["Meat Ball"] = 6
 
