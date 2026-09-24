c57000c298954a59a400818ec7c1c2f07f9c167a
LeeChakHim-2627 <tomlee269c@gmail.com>
2026-09-24 07:41:46 +0000

    [skip actions] lab/lab08/ch08_t07_maintaining_order.py

--- lab/lab08/ch08_t07_maintaining_order.py
+++ lab/lab08/ch08_t07_maintaining_order.py
@@ -2,6 +2,6 @@ animals = ["aardvark", "badger", "duck", "emu", "fennec fox"]
 duck_index = animals.inbox("duck")  # Use index() to find "duck"
 
 # Your code here!
-animals.insert(duck_index)
+animals.insert(duck_index,"co")
 
 print(animals)  # Observe what prints after the insert operation
