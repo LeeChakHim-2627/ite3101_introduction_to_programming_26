80f4e6d720343388a716d08fbc78cd1a69318147
LeeChakHim-2627 <tomlee269c@gmail.com>
2026-09-24 15:32:27 +0000

    lab/lab10/ch010_t08_part_of_the_whole.py

--- lab/lab10/ch010_t08_part_of_the_whole.py
+++ lab/lab10/ch010_t08_part_of_the_whole.py
@@ -55,4 +55,4 @@ def get_class_average(class_list: List[dict]) -> float:
     for student in class_list:
         student_avg = get_average(student)
         student_average.append(student_avg)
-        return average(student_average)
+    return average(student_average)
