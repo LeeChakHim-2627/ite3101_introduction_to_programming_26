caabe532b03ab72c003ae2dc97120c01ba318e7b
LeeChakHim-2627 <tomlee269c@gmail.com>
2026-09-24 15:20:34 +0000

    [skip actions] lab/lab10/ch010_t07_sending_a_letter.py

--- lab/lab10/ch010_t07_sending_a_letter.py
+++ lab/lab10/ch010_t07_sending_a_letter.py
@@ -41,7 +41,7 @@ def get_average(student: dict) -> float:
     return 0.1 * homework + 0.3 * quizzes + 0.6 * tests
 
 # Add your function below!
-def get_letter_grade(score: float) -> float:
+def get_letter_grade(score: float) -> str:
     if score >= 90:
         return "A"
     elif score >= 80:
@@ -51,4 +51,4 @@ def get_letter_grade(score: float) -> float:
     elif score >= 60:
         return "D"
     else:
-        return "F"
+        return False
