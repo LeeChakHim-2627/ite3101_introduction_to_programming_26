0ba93bb133da700549c80b6496dc28e1897568f4
LeeChakHim-2627 <tomlee269c@gmail.com>
2026-09-24 07:53:12 +0000

    lab/lab08/ch08_t13_remove_few_things.py

--- lab/lab08/ch08_t12_change_yr_mind.py
+++ lab/lab08/ch08_t12_change_yr_mind.py
@@ -13,6 +13,6 @@ del zoo_animals['Unicorn']
 del zoo_animals['Sloth']
 del zoo_animals['Bengal Tiger']
 
-zoo_animals['Rockhopper Penguin'] = "Unknow"
+zoo_animals['Rockhopper Penguin'] = "Unknown Place"
 
 print(zoo_animals)
