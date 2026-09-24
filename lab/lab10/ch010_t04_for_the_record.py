fa1821931f051db14fffe76126c83cc16675a108
LeeChakHim-2627 <tomlee269c@gmail.com>
2026-09-24 08:53:05 +0000

    lab/lab10/ch010_t04_for_the_record.py

--- lab/lab10/ch010_t04_for_the_record.py
+++ lab/lab10/ch010_t04_for_the_record.py
@@ -21,5 +21,5 @@ students = [lloyd, alice, tyler]
 for student in students:
     print(student["name"])
     print(student["homework"])
-    print(student["q"])
-    print(student[""])
+    print(student["quizzes"])
+    print(student["tests"])
