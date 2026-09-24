2011cdcb27f49b8ba0705b0973acf20d1326491b
LeeChakHim-2627 <tomlee269c@gmail.com>
2026-09-24 07:50:15 +0000

    [skip actions] lab/lab08/ch08_t11_new_entries.py

--- lab/lab08/ch08_t11_new_entries.py
+++ lab/lab08/ch08_t11_new_entries.py
@@ -4,8 +4,8 @@ print(menu['Chicken Alfredo'])
 
 # Your code here: Add some dish-price pairs to menu!
 meuu["Icecream"] = 3
-meuu["Icecream"] = 3
-meuu["Icecream"] = 3
+meuu["Spam"] = 2.5
+meuu["Meat Ball"] = 6
 
 print("There are " + str(len(menu)) + " items on the menu.")
 print(menu)
